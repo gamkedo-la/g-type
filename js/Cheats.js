7d8e5acb0b4caeef63d037fe0e5e7de7d106f82e
vmckeown <vmckeown79@gmail.com>
2018-10-23 11:17:26 -0400

    Update js/Cheats.js
    Just practicing my first Git Commit.  I will remove the update soon.

--- js/Cheats.js
+++ js/Cheats.js
@@ -3,6 +3,8 @@ function Cheats() {
     //Keep global for now
 
     //TODO: Add function that console.logs which cheats are active to call at program start
+	
+	//Hello World, practicing using Git.  I will remove.  -Vince McKeown
     this.documentDebugKeysIfEnabled = function() {
         if(this.debugKeysEnabled) {
             console.log("Debug Keys Enabled: ");
