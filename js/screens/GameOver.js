139deba188784bd7d9889894bc0844b427d464d4
H Trayford <bowman_trayford@hotmail.com>
2018-11-30 14:45:14 +0300

    Game Over Text added to GameOver Screen.

--- js/screens/GameOver.js
+++ js/screens/GameOver.js
@@ -132,13 +132,18 @@ function GameOverScreen() {
 
 		canvasContext.drawImage(gameFrame1, 0, 0, gameFrame1.width, gameFrame1.height, 0, 0, canvas.width, canvas.height);
 
-		gameFont.printTextAt(textStrings.GameOver, {x:GameField.midX - 150, y:GameField.y - 20}, 35, textAlignment.Left);
+		gameFont.printTextAt(textStrings.GameOver, {x:GameField.midX - 150, y:GameField.y - 10}, 35, textAlignment.Left);
+		
+		gameFont.printTextAt(textStrings.GameOverScript1, {x:GameField.midX, y:GameField.midY - 40}, 30, textAlignment.Center);
+		gameFont.printTextAt(textStrings.GameOverScript2, {x:GameField.midX, y:GameField.midY}, 30, textAlignment.Center);
+		gameFont.printTextAt(textStrings.GameOverScript3, {x:GameField.midX, y:GameField.midY + 40}, 30, textAlignment.Center);
+		
         drawHighScores();
 	};
 
     const drawHighScores = function() {
         for(var i = 0; i < allHighScores.length; i++){
-            gameFont.printTextAt((i + 1) + "." + allHighScores[i],  {x:GameField.midX - 120, y:GameField.y + GameField.height / 2 - 150 + i * 50 }, 35, textAlignment.Left); 
+            gameFont.printTextAt((i + 1) + "." + allHighScores[i],  {x:GameField.midX - 120, y:GameField.y + 75 + (i * 50)}, 35, textAlignment.Left); 
         }        
     }
 
