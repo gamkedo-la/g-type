ad957d6fc0c75c286db8505118809a1d56ebba9c
Kirvee <Powerpufprin8@aol.com>
2018-12-05 14:04:44 -0500

    Added frames to both light and dark UI images to slow them down, changed the appropriate values in the JS files, created a bug where the Ghost Ship sprite clips through the UI and is moving weirdly....

--- js/UIEntities/UIForce.js
+++ js/UIEntities/UIForce.js
@@ -3,8 +3,8 @@ function UIForce(position = {x:0, y:0}) {
     this.position = {x:position.x, y:position.y};
     this.isLit = false;
     const SPRITE_SCALE = 1.75;
-    const lightSprite = new AnimatedSprite(lightForceUI, 2, 50, 50, true, true, {min:0, max:0}, 0, {min:0, max:1}, 512, {min:1, max:1}, 0);
-    const darkSprite = new AnimatedSprite(darkForceUI, 2, 50, 50, true, true, {min:0, max:0}, 0, {min:0, max:1}, 512, {min:1, max:1}, 0);
+    const lightSprite = new AnimatedSprite(lightForceUI, 4, 50, 50, true, true, {min:0, max:0}, 0, {min:0, max:3}, 512, {min:3, max:3}, 0);
+    const darkSprite = new AnimatedSprite(darkForceUI, 6, 50, 50, true, true, {min:0, max:0}, 0, {min:0, max:5}, 512, {min:5, max:5}, 0);
     
     this.size = {width:SPRITE_SCALE * lightSprite.width, height:SPRITE_SCALE * lightSprite.height};
     
