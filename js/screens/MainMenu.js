5f4faa0bab5ec3bd99bc0a84ad664d574e27e4c1
Chris DeLeon <gamkedo@gmail.com>
2018-12-06 12:02:17 -0800

    Restored Christer's 7cfd321 menu player ship thruster effects

--- js/screens/MainMenu.js
+++ js/screens/MainMenu.js
@@ -61,7 +61,7 @@ function MenuScreen() {
             }
         }
         
-        if ((this.keysPressed(KEY_CTRL, KEY_ALT, KEY_SHIFT, KEY_D))) {            
+        if ((this.keysPressed(KEY_CTRL, KEY_ALT, KEY_SHIFT, KEY_D))) {
             cheats.debugKeysEnabled = !cheats.debugKeysEnabled;
             if (cheats.debugKeysEnabled) {
                 cheats.printCheatKeysInstructions();
@@ -153,13 +153,13 @@ function MenuScreen() {
 
 		if(timeSinceKey > 5000) {
 			timeSinceKey = 0;
-			
+
 			if(lastShowedDemo) {
 				ScreenStates.setState(STORY_SCENE_SCREEN, 1);
 			} else {
 				ScreenStates.setState(DEMO_SCENE_SCREEN, 1);
 			}
-			
+
 			lastShowedDemo = !lastShowedDemo;
 		}
 
@@ -180,7 +180,7 @@ function MenuScreen() {
 
         // render menu
         printMenu(selections, selectorPositionIndex);
-		
+
 		//draw the thruster
 		let thrusterMod = timer.getCurrentTime() % 16 < 8 ? 0 : 3;
 		thrusterPosition.x = selectorPosition.x - 28 + thrusterMod;
@@ -188,6 +188,10 @@ function MenuScreen() {
 		//this.thrusterSize.width = thrusterSprite.width * thrusterMod;
 		thrusterSprite.drawAt(thrusterPosition.x, thrusterPosition.y, thrusterSize.width, thrusterSize.height);
 		
+        // also emit some thruster particles
+        trailEmitter = createParticleEmitter(thrusterPosition.x+28, thrusterPosition.y+16, playerTrail);
+        ParticleRenderer.renderAll(canvasContext);
+
         //draw selector sprite
         selectorSprite.drawAt(selectorPosition.x, selectorPosition.y, 52, 32);
 	};
